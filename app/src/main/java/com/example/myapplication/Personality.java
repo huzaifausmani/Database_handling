83080991158e89d38b1cd7bbf1e37b6efaaf4e69
HuzaifaUsmani <huzaifa.1019@gmail.com>
2023-01-11 10:22:39 +0500

    C-2 ViewClass Personality

--- app/src/main/java/com/example/myapplication/Personality.java
+++ app/src/main/java/com/example/myapplication/Personality.java
@@ -1,4 +1,21 @@
 package com.example.myapplication;
 
 public class Personality {
+    public String name;
+    public int introvert;
+    public int extrovert;
+    public int ambivert;
+    public int psychopath;
+    public int sociopath;
+
+    public Personality(String name, int introvert, int extrovert, int ambivert, int psychopath, int sociopath){
+        this.name = name;
+        this.introvert = introvert;
+        this.extrovert = extrovert;
+        this.ambivert = ambivert;
+        this.psychopath = psychopath;
+        this.sociopath = sociopath;
+    }
+    
+
 }
