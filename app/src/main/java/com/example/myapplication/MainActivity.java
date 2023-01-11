996ea061b623c416a09913aadae84315678c76a3
HuzaifaUsmani <huzaifa.1019@gmail.com>
2023-01-11 11:07:54 +0500

    C-4 Main-Activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,10 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    Database db;
+    Personality personality;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db = new Database(this);
+        personality = new Personality("Huzaifa", 4,1,1,1,3);
+
+        db.insertOne(personality);
     }
 }
